e889cfa72c6b581941e21b45a95cafb7ffdb9c46
Your Name <yourname@example.com>
2024-10-22 13:31:34 +0800

    FINISHED CALCULATOR, ADDED IF STATEMENTS TO HELP

--- script.js
+++ script.js
@@ -4,18 +4,54 @@ let step = 0
 let operation
 let result = 0
 
-function getNumber(num) {
+let numArray = []
+let secondNumArray = []
 
+let display = document.querySelector('.input')
+
+display.value = 0;
+
+function getNumber(num) {
+    if(step === 0 || step === 1) {
+        numArray.push(num);
+        step = 1;
+        firstNumber = Number(numArray.join(''));
+        display.value = firstNumber;
+    } else if (step === 2) {
+        secondNumArray.push(num)
+        secondNumber = Number(secondNumArray.join(''));
+        display.value = secondNumber;
+    }
 }
 
 function getOperator(op) {
-
+    step = 2;
+    operation = op;
 }
 
 function clearDisplay() {
-    
+    display.value = 0;
+    firstNumber = null;
+    secondNumber = null;
+    step = 0
+    operation = null
+    result = 0
+    numArray = []
+    secondNumArray = []
 }
 
-const calculateEquals = () => {
-
+const calculate = () => {
+    if(operation === '+') {
+        result = firstNumber + secondNumber
+        display.value = result
+    } else if (operation === '-') {
+        result = firstNumber - secondNumber
+        display.value = result
+    } else if (operation === 'x') {
+        result = firstNumber * secondNumber
+        display.value = result
+    } else if (operation === '/') {
+        result = firstNumber / secondNumber
+        display.value = result;
+    }
 }
